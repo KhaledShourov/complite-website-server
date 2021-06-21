9307baad46b5f33c8f6338dfc41d980a3cb4bd3f
Your Name <you@example.com>
2021-06-21 21:47:13 +0600

    Complite

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.jmtl7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jmtl7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 
 const app = express()
 
@@ -23,5 +17,21 @@ const port = 5000
 app.get('/', (req, res) =>{
   res.send('Database Working')
 })
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  const orderCollection = client.db("tech-master").collection("orderProducts");
+
+  app.post('/addOrder', (req, res) =>{
+    const order = res.body;
+    orderCollection.insertOne(order)
+    .then(result =>{
+      res.send(res.listenerCount)
+    })
+  })
+});
 
 app.listen(process.env.PORT || port)
